57c1e3be8a428ce3149c05eb0cdffd882b5b5174
LegoBatman <florian.staeps@haw-hamburg.de>
2016-11-09 09:57:45 +0100

    mehr fertig als vorher
    - Logging ergänzt
    - kleine Fehlerkorrekturen
    - unnötigen Quellcode entfernt

--- Termin 2/ggt_process.erl
+++ Termin 2/ggt_process.erl
@@ -8,37 +8,25 @@
 
 % Initialisiert den ggt-prozess
 init(Delay, WorkTime, ClientName, NameServiceName, CoordinatorName, Quota) ->
-    % Den NameService anpingen, beim NameService registrieren und anschließend den Coordinator abfragen
-%    net_adm:ping(NameServiceNode),
-    % Fürs Sync wichtig
-%    timer:sleep(500),
 
     NameService = global:whereis_name(NameServiceName),
     NameService ! {self(), {rebind, ClientName, node()}},
 
     receive 
       ok  -> 
+        LogFile = tool:format("~p.log",[ClientName]),
+        tool:l(LogFile,ClientName,"ggt_process wird gestartet "),
 
-      LogFile = tool:format("~p.log",[ClientName]),
-      tool:l(LogFile,ClientName,"ggt_process wird gestartet "),
-
-     % spawn(fun() -> handleKillCommand() end),
-     % spawn(fun() -> handlePing(ClientName) end),
      register(ClientName, self()),
 
       tool:send(CoordinatorName,NameServiceName,{hello, ClientName}),
       tool:l(LogFile,ClientName,"Beim Koordinator anmelden "),
-      workPhase(ClientName,false,noTimer_xD, CoordinatorName, NameServiceName,
-                 'LeftNeighbor noch nicht gesetzt',
-                 'RightNeighbor noch nicht gesetzt',
-                  WorkTime, Delay, Quota,
-                  'Mi noch nicht gesetzt',
-                  LogFile)
+      workPhase(ClientName, false, noTimer_xD, CoordinatorName, NameServiceName, 'LeftNeighbor noch nicht gesetzt', 'RightNeighbor noch nicht gesetzt', WorkTime, Delay, Quota, 'Mi noch nicht gesetzt', LogFile)
     end. 
 
 % ######################################################## Berechnung / Arbeitsphase ########################################################## %
 
-workPhase(ClientName,Consense,Timer, Coordinator, NameService, LeftNeighbor, RightNeighbor, WorkTime, Delay, ProcessCountNeeded, Mi, LogFile) ->
+workPhase(ClientName, Consense, Timer, Coordinator, NameService, LeftNeighbor, RightNeighbor, WorkTime, Delay, ProcessCountNeeded, Mi, LogFile) ->
   receive
     {setneighbors, Left, Right} ->
       tool:l(LogFile,ClientName,"Setze Rechten und Linken Nachbarn : Links- ~p , Rechts- ~p  ", [Left,Right]),
@@ -46,13 +34,13 @@ workPhase(ClientName,Consense,Timer, Coordinator, NameService, LeftNeighbor, Rig
     {setpm, NMi} ->
       tool:l(LogFile,ClientName,"Mi erhalten | Zeitstempel:  ~p ", [erlang:system_time()]),
       timer:cancel(Timer),
-      NewTimer = timer:send_after(round(WorkTime/2*1000)),{self(),timeout}),
+      NewTimer = timer:send_after(round(WorkTime/2*1000), {self(),timeout}),
       workPhase(ClientName,false,NewTimer, Coordinator, NameService, LeftNeighbor, RightNeighbor, WorkTime, Delay, ProcessCountNeeded, NMi, LogFile);
     {sendy, Y} ->
       tool:l(LogFile,ClientName,"Y erhalten | Zeitstempel:  ~p ", [erlang:system_time()]),
       NewMi = calc(Mi, Y, LeftNeighbor, RightNeighbor, Coordinator, Delay, ClientName, NameService, LogFile),
       timer:cancel(Timer),
-      NewTimer = timer:send_after(round(WorkTime/2*1000)),{self(),timeout}),
+      NewTimer = timer:send_after(round(WorkTime/2*1000),{self(),timeout}),
       workPhase(ClientName,false,NewTimer, Coordinator, NameService, LeftNeighbor, RightNeighbor, WorkTime, Delay, ProcessCountNeeded, NewMi, LogFile);
 
      {From, tellmi} ->
@@ -65,29 +53,28 @@ workPhase(ClientName,Consense,Timer, Coordinator, NameService, LeftNeighbor, Rig
         tool:l(LogFile,ClientName,"Senden Ping an Koordinator | Zeitstempel:  ~p ", [erlang:system_time()]),
         workPhase(ClientName,Consense,Timer, Coordinator, NameService, LeftNeighbor, RightNeighbor, WorkTime, Delay, ProcessCountNeeded, Mi, LogFile);
       
-   
-
-      {From, {vote, Name}} ->
+      {From, {vote, _}} ->
         if
           Consense ->
-            From ! {voteYes, ClientName}  ;  
+            tool:l(LogFile,ClientName,"Stimme abbruch der Berechnung zu | Zeitstempel:  ~p ", [erlang:system_time()]),
+            From ! {voteYes, ClientName};  
           true ->
-            workPhase(ClientName,true,Timer, Coordinator, NameService, LeftNeighbor, RightNeighbor, WorkTime, Delay, ProcessCountNeeded, Mi, LogFile);
+            workPhase(ClientName, true, Timer, Coordinator, NameService, LeftNeighbor, RightNeighbor, WorkTime, Delay, ProcessCountNeeded, Mi, LogFile)
         end;
-
 
       timeout -> 
         if 
           Consense -> 
-            timer:send_after(Delay*4000,{spawn(fun() -> startVote(ClientName,Koordinator,NameService,Mi,Quota) end),timeout});
+            tool:l(LogFile,ClientName,"Starte Abstimmung | Zeitstempel:  ~p ", [erlang:system_time()]),
+            timer:send_after(Delay*4000,{spawn(fun() -> startVote(ClientName, Coordinator, NameService, Mi, ProcessCountNeeded) end),timeout}),
             workPhase(ClientName,true,Timer, Coordinator, NameService, LeftNeighbor, RightNeighbor, WorkTime, Delay, ProcessCountNeeded, Mi, LogFile);
-          true -> 
-            workPhase(ClientName,true,Timer, Coordinator, NameService, LeftNeighbor, RightNeighbor, WorkTime, Delay, ProcessCountNeeded, Mi, LogFile);
+          true ->
+            NewTimer = timer:send_after(round(WorkTime/2*1000),{self(),timeout}),
+            workPhase(ClientName,true, NewTimer, Coordinator, NameService, LeftNeighbor, RightNeighbor, WorkTime, Delay, ProcessCountNeeded, Mi, LogFile)
         end;
 
         
       kill ->
-        %exit("Kill Command"),
         timer:cancel(Timer),
         tool:l(LogFile,ClientName,"Beende ggt_process "),
         NameServicePID = global:whereis_name(NameService),
@@ -121,8 +108,6 @@ startVote(ClientName,Koordinator,NameService,Mi,Quota) ->
   receive
     {From, {voteYes, _}} ->
       startVote(ClientName,Koordinator,NameService,Mi,Quota-1);
-      %looooging
     timeout -> 
-      %loging
       ok
   end.
